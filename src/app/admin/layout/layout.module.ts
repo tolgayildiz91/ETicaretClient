4065ad875dd15279c0760c499ac8d275d2e82836
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-10-12 23:30:55 +0300

    layout

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 
 
@@ -11,7 +14,9 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    RouterModule,
+    MatSidenavModule
   ],
   exports:[
     LayoutComponent
