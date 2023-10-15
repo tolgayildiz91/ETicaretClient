d0e7a8413910b013c00df311ad77473ebce169fd
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-10-15 23:51:16 +0300

    service

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -9,7 +10,7 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 })
 export class ProductsComponent extends BaseComponent implements OnInit{
 
-  constructor(spinner : NgxSpinnerService) {
+  constructor(spinner : NgxSpinnerService,private httpClientService:HttpClientService) {
     super(spinner);
     
   }
@@ -17,6 +18,9 @@ export class ProductsComponent extends BaseComponent implements OnInit{
   ngOnInit(): void {
       
     this.showSpinner(SpinnerType.BallAtom)
+    this.httpClientService.get({
+      controller:"products"
+    }).subscribe(data=> console.log(data));
   }
 
 
