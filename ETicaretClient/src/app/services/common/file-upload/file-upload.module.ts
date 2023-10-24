f7e1f244b10f77a01c780f2785da9a33ca851c20
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-10-24 22:04:22 +0300

    update

--- ETicaretClient/src/app/services/common/file-upload/file-upload.module.ts
+++ ETicaretClient/src/app/services/common/file-upload/file-upload.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileUploadComponent } from './file-upload.component';
 import { NgxFileDropModule } from 'ngx-file-drop';
+import { DialogModule } from '../../../dialogs/dialog.module';
 import { FileUploadDialogComponent } from '../../../dialogs/file-upload-dialog/file-upload-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
